91dd39ff8b345494b41c9bcc16cfc973a428b6e0
billClinton048 <clintonkaranja46@gmail.com>
2025-03-12 01:27:34 +0300

    Added the Top Nav

--- app/src/App.vue
+++ app/src/App.vue
@@ -1,16 +1,11 @@
 <script setup>
+import TopNav from '@/components/TopNav.vue'
 
 </script>
 
 <template>
- <div class="bg-orange-400 h-screen ">
-<h1 class="text-5xl max-w-5xl mx-auto py-10 text-center">
-  Welcome to Neo Space we create all solutions for you.
-  <br><span class="text-2xl">#It aint Free.</span>
-  <br><span class="text-4xl text-white">This here is for <span class="text-purple-200 ">Mamuza</span>.</span>
-</h1>
+<TopNav />
 
- </div>
 </template>
 
 <style scoped>
