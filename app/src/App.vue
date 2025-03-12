2b6b444eaa30d16b4ac10815b26c8f88e214454f
billClinton048 <clintonkaranja46@gmail.com>
2025-03-12 05:04:04 +0300

    message

--- app/src/App.vue
+++ app/src/App.vue
@@ -1,10 +1,12 @@
 <script setup>
 import TopNav from '@/components/TopNav.vue'
+import HeroSection from './components/HeroSection.vue';
 
 </script>
 
 <template>
 <TopNav />
+<HeroSection />
 
 </template>
 
